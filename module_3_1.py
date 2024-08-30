ef63f69822dee9a3c6ecea2e279148eda869018e
mnikodim <Marinenik@yandex.ru>
2024-08-30 16:41:56 +0300

    functions

---
+++ module_3_1.py
@@ -0,0 +1,24 @@
+calls = 0
+string = "How much"
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list_to_search):
+    count_calls()
+    return string.lower() in [s.lower() for s in list_to_search]
+
+
+print(string_info("AbraKadabRa"))
+print(string_info("URBAN"))
+print(is_contains("URbAN", ["ur", "RBAN", "ban"]))
+print(is_contains("chemodAN", ["chem", "oda", "chemodan"]))
+print(calls)
+
+
