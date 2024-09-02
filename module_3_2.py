d77a78b915c1c0c64c7a37d16bfc7e9d1ba9c99b
mnikodim <Marinenik@yandex.ru>
2024-09-02 17:09:15 +0300

    check function call(final)

--- module_3_2.py
+++ module_3_2.py
@@ -1,18 +1,15 @@
 def send_email(massage, recipient, *, sender="university.help@gmail.com"):
-    index = recipient.find("@") and sender.find("@")
-    variants = (".com", ".ru", ".net")
-    if sender.endswith(variants) or recipient.endswith(variants):
-        if index != -1:
-            print("Письмо успешно отправлено с адреса university.help@gmail.com на адрес vasyok1337@gmail.com")
+    if "@" in recipient and "@" in sender:
+        variants = (".com",".ru",".net")
+        if any(sender.endswith(variant) for variant in variants) and any(recipient.endswith(variant) for variant in variants):
+            if sender == recipient:
+                print("Нельзя отправить письмо самому себе!")
+            if sender != "university.help@gmail.com":
+                print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+            else:
+                print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
         else:
-            print("Невозможно отправить письмо с адреса", sender, "на адрес", recipient, ".")
-
-        if sender == recipient:
-            print("Нельзя отправить письмо самому себе!")
-            if sender == "university.help@gmail.com":
-                print("Письмо успешно отправлено с адреса", sender, "на адрес", recipient, ".")
-        else:
-            print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient, ".")
+            print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient} .")
 
 
 send_email('Это сообщение для проверки связи,', 'vasyok1337@gmail.com')
