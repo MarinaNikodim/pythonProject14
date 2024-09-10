bd58c00422123883a84b415f08448842db23657f
mnikodim <Marinenik@yandex.ru>
2024-09-10 20:58:42 +0300

    recurtion. 2nd vertion

--- module_3_4.py
+++ module_3_4.py
@@ -3,6 +3,8 @@ def single_root_words(root_word, *other_words):
     for i in other_words:
         if root_word.lower() in i.lower():
             same_words.append(i)
+        elif i.lower() in root_word.lower():
+            same_words.append(i)
     return same_words
     #return [i for i in other_words if root_word.lower() in i.lower()]
 
